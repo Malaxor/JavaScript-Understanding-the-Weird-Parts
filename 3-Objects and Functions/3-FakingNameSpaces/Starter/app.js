0c01cbde53c77b38fec8c890b2076c2a49622d07
Malaxor <nogobaduk@gmail.com>
2019-03-04 09:41:45 -0700

    faking namespaces

--- 3-Objects and Functions/3-FakingNameSpaces/Starter/app.js
+++ 3-Objects and Functions/3-FakingNameSpaces/Starter/app.js
@@ -2,3 +2,12 @@ var greet = 'Hello!';
 var greet = 'Hola!'; 
 
 console.log(greet);
+
+const english = {};
+const spanish = {};
+
+english.greet = 'Hello';
+spanish.greet = 'Hola';
+
+console.log(english);
+console.log(spanish);
