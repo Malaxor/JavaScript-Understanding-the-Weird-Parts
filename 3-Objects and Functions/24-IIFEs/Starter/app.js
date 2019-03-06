456e323db4f42683d5f471a1577e4146ab9f309d
Malaxor <nogobaduk@gmail.com>
2019-03-06 16:21:01 -0700

    IFFE

---
+++ 3-Objects and Functions/24-IIFEs/Starter/app.js
@@ -0,0 +1,15 @@
+
+// Immediately Invoked Function Expression (IFFE)
+var greeting = function(name) {
+
+   return `Hello, ${name}.`;
+}('Phil');
+
+console.log(greeting);
+
+var firstName = 'Johnny';
+
+(function(name) {
+	var greeting = 'Hello';
+   console.log(`${greeting}, ${name}. I'm a function expression.`)
+}(firstName));
