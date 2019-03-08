15f8c2a3978936d0a5c6963ce9ad1ac907802549
Malaxor <nogobaduk@gmail.com>
2019-03-08 15:50:49 -0700

    call, apply, bind

--- 3-Objects and Functions/30-CallApplyBind/Starter/app.js
+++ 3-Objects and Functions/30-CallApplyBind/Starter/app.js
@@ -1,50 +1,52 @@
 
 const person = {
 
-    firstName: 'John',
-    lastName: 'Doe',
-    getFullName: function() {
+	firstName: 'John',
+	lastName: 'Doe',
+	getFullName() {
 
-        const fullName = `${this.firstName} ${this.lastName}`;
-        return fullName;
-    }
+		const fullName = `${this.firstName} ${this.lastName}`;
+		return fullName;
+	}
 };
 
 const logName = function(lang1, lang2) {
 
-    console.log(`Logged: ${this.getFullName()}`);
-    console.log(`Arguments: ${lang1} ${lang2}`);
-    console.log('----------------------------');
+	console.log(`Logged: ${this.getFullName()}`);
+	console.log(`Arguments: ${lang1} ${lang2}`);
+	console.log('----------------------------');
 }
 // bind creates a copy of the function
 const logPersonName = logName.bind(person);
 
-logPersonName();
+logPersonName('en', 'es');
 // calls executes the function, with the first argument being an object
 logName.call(person, 'en', 'es');
 // apply behaves like call but needs an array of arguments, excluding the first 
 logName.apply(person, ['en', 'es']);
-// example of IIFE with apply (works for bind and call)
+// example of IIFE with apply (also works with call)
 (function(lang1, lang2) {
 
-    console.log(`Logged: ${this.getFullName()}`);
-    console.log(`Arguments: ${lang1} ${lang2}`);
-    console.log('-------------------------');
-}).apply(person, ['en', 'es']);
+	console.log('With dot apply/call attached directly to function');
+	console.log(`Logged: ${this.getFullName()}`);
+	console.log(`Arguments: ${lang1} ${lang2}`);
+	console.log('-------------------------');
+}).call(person, 'en', 'es');
 
 //function borrowing
 const person2 = {
 
-    firstName: 'Jane',
-    lastName: 'Doe'
+   firstName: 'Jane',
+   lastName: 'Doe'
 };
 console.log(person.getFullName.apply(person2));
 
-// function currying
+// function currying (creating a copy of a function with preset parameters)
 function multiply(a, b) {
 
-    return a * b;
+   return a * b;
 }
 // in this case, bind sets the first parameter to 2
+// you could hardcode any and all parameters
 const multiplyByTwo = multiply.bind(this, 2);
 console.log(multiplyByTwo(4));
