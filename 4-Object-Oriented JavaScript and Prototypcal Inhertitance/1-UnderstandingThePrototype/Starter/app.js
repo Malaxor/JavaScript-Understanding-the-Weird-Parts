cb104e21f5f52507bb36e8cd98484e380c29cb90
Malaxor <nogobaduk@gmail.com>
2019-03-11 18:00:14 -0700

    understanding the prototype

--- 4-Object-Oriented JavaScript and Prototypcal Inhertitance/1-UnderstandingThePrototype/Starter/app.js
+++ 4-Object-Oriented JavaScript and Prototypcal Inhertitance/1-UnderstandingThePrototype/Starter/app.js
@@ -1,29 +1,26 @@
 
 const person = {
 
-    firstName: 'Default',
-    lastName: 'Default',
-    getFullName() {
-        return `${this.firstName} ${this.lastName}`
-    }
+	firstName: 'Default',
+	lastName: 'Default',
+	getFullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
 };
 
 const john = {
 
-    firstName: 'John',
-    lastName: 'Doe'
+	firstName: 'John',
+	lastName: 'Doe'
 };
-// jon now inherits all of person's properties/methods
-// the prototype object will not override other object's identically named properties (prototype chain)
+
 john.__proto__ = person;
 console.log(john.getFullName());
-console.log(john.firstName);
-console.log('-------------------');
 
 const jane = {
 
-    firstName: 'Jane'
-}
+	firstName: 'Jane'
+};
+
 jane.__proto__ = person;
 console.log(jane.getFullName());
-console.log(jane.lastName);
