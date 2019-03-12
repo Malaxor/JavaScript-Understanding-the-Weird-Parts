a5e05479cd17da0a96fc7f9d472fb0f952cc2c1d
Malaxor <nogobaduk@gmail.com>
2019-03-12 09:17:27 -0700

    function constructor and prototype

--- 5-Builiding Objects/2-FunctionConstructorsAndPrototype/Starter/app.js
+++ 5-Builiding Objects/2-FunctionConstructorsAndPrototype/Starter/app.js
@@ -1,17 +1,15 @@
 function Person(firstname, lastname) {
  
-    console.log(this);
-    this.firstname = firstname;
-    this.lastname = lastname;
-    console.log('This function is invoked.');
+	console.log(this);
+	this.firstname = firstname;
+	this.lastname = lastname;
+	console.log('This function is invoked.');
 }
 
-Person.prototype.getFullName = () => {
+Person.prototype.getFullName = function() {
 
-   return `${this.firstname} ${this.lastname}`
+	return `${this.firstname} ${this.lastname}`
 };
 
-var john = new Person('John', 'Doe');
-
-var jane = new Person('Jane', 'Doe');
+const jane = new Person('Jane', 'Doe');
 console.log(jane);
