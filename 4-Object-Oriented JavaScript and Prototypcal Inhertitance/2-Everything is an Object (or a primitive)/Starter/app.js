61f0c24862c923f852e4d92b63c8d03f098e178c
Malaxor <nogobaduk@gmail.com>
2019-03-11 19:18:29 -0700

    reflection and extend

---
+++ 4-Object-Oriented JavaScript and Prototypcal Inhertitance/2-Everything is an Object (or a primitive)/Starter/app.js
@@ -0,0 +1,4 @@
+
+const a = {};
+const b = function() {};
+const c = [];
---
+++ 4-Object-Oriented JavaScript and Prototypcal Inhertitance/3-ReflectionAndExtend/Starter/app.js
@@ -0,0 +1,40 @@
+
+const person = {
+
+	firstName: 'Default',
+	lastName: 'Default',
+	getFullName() {
+		return `${this.firstName} ${this.lastName}`;
+	}
+};
+
+const john = {
+
+	firstName: 'John',
+	lastName: 'Doe'
+};
+
+john.__proto__ = person;
+
+for(let prop in john) {
+
+	john.hasOwnProperty(prop) && console.log(`${prop}: ${john[prop]}`);	
+}
+
+const jane = {
+
+	address: '111 Main St.',
+	getFormalFullName() {
+		return `${this.lastName}, ${this.firstName}`;
+	}
+};
+
+const jim = {
+
+	getFirstName() {
+		return firstName;
+	}
+}
+
+_.extend(john, jane, jim);
+console.log(john);
