06329c0c8278708b0a87847fc5e6b3b4ecc6cb54
Malaxor <nogobaduk@gmail.com>
2019-03-13 08:26:17 -0700

    strict mode

---
+++ 6-Odds and Ends/3-strictmode/Starter/app.js
@@ -0,0 +1,14 @@
+
+function logNewPerson() {
+   'use strict';
+   
+   var person2;
+   persom2 = {};
+   console.log(persom2);
+}
+
+var person;
+// have to declare a variable, in strict mode, before initializing it
+persom = {};
+console.log(persom);
+logNewPerson();
