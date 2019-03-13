c5b5f501d7dca5dcda1fecdd0bc2974f73cb365a
Malaxor <nogobaduk@gmail.com>
2019-03-13 08:18:59 -0700

    typeof and instanceof

--- 6-Odds and Ends/2-typeofinstanceof/app.js
+++ 6-Odds and Ends/2-typeofinstanceof/app.js
@@ -1,27 +1,27 @@
-var a = 3;
+
+const a = 3;
 console.log(typeof a);
 
-var b = "Hello";
+const b = 'Hello';
 console.log(typeof b);
 
-var c = {};
+const c = {};
 console.log(typeof c);
 
-var d = [];
-console.log(typeof d); // weird!
-console.log(Object.prototype.toString.call(d)); // better!
+const d = [];
+console.log(typeof d);
+console.log(Object.prototype.toString.call(d));
 
 function Person(name) {
-    this.name = name;
+   this.name = name;
 }
 
-var e = new Person('Jane');
+const e = new Person('Jane');
 console.log(typeof e);
 console.log(e instanceof Person);
 
-console.log(typeof undefined); // makes sense
-console.log(typeof null); // a bug since, like, forever...
+console.log(typeof undefined);
+console.log(typeof null);
 
-var z = function() { };
+const z = function() {};
 console.log(typeof z);
-
